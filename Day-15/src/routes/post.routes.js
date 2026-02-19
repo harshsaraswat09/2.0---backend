f416999c6165dcb2254244b5d230d8bedab34346
Harsh Saraswat <harshsaraswat09@gmail.com>
2026-02-19 17:34:46 +0530

    Implement user authentication middleware and refactor post controllers to utilize user data from the middleware

--- Day-15/src/routes/post.routes.js
+++ Day-15/src/routes/post.routes.js
@@ -4,24 +4,26 @@ const postController = require("../controllers/post.controller")
 const multer = require("multer")
 const upload = multer({ storage: multer.memoryStorage()})
 
+const identifyUser = require('../middlewares/auth.middleware')
+
 
 /**
  * POST /api/posts [protected]
  * - req.body = { caption, image-file }
  */
-postRouter.post('/',upload.single("image"),postController.createPostController)
+postRouter.post('/',upload.single("image"), identifyUser,postController.createPostController)
 
 
 /**
  * GET /api/posts [protected]
  */
-postRouter.get("/",postController.getPostController)
+postRouter.get("/", identifyUser, postController.getPostController)
 
 
 /**
  * GET /api/posts/details/:postid
  * - return an detail about specific post with the id. Also check whether the post belongs to the user that request comes from
  */
-postRouter.get("/details/:postId", postController.getPostDetailsController)
+postRouter.get("/details/:postId", identifyUser,  postController.getPostDetailsController)
 
 module.exports = postRouter 
