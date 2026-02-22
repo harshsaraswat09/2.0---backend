36422ad04f88a532d7966b2c67d1d9336f6ccc0a
Harsh Saraswat <harshsaraswat09@gmail.com>
2026-02-22 21:45:34 +0530

    Integration Initialization

---
+++ Day-19/Backend/Day-15/src/routes/post.routes.js
@@ -0,0 +1,36 @@
+const express = require("express")
+const postRouter = express.Router()
+const postController = require("../controllers/post.controller")
+const multer = require("multer")
+const upload = multer({ storage: multer.memoryStorage()})
+
+const identifyUser = require('../middlewares/auth.middleware')
+
+
+/**
+ * POST /api/posts [protected]
+ * - req.body = { caption, image-file }
+ */
+postRouter.post('/',upload.single("image"), identifyUser,postController.createPostController)
+
+
+/**
+ * GET /api/posts [protected]
+ */
+postRouter.get("/", identifyUser, postController.getPostController)
+
+
+/**
+ * GET /api/posts/details/:postid
+ * - return an detail about specific post with the id. Also check whether the post belongs to the user that request comes from
+ */
+postRouter.get("/details/:postId", identifyUser,  postController.getPostDetailsController)
+
+/**
+ * @route POST /api/posts/like/:postid
+ */
+
+postRouter.post('/like/:postId', identifyUser, postController.likePostController)
+
+
+module.exports = postRouter 
---
+++ Day-19/Frontend/src/features/auth/pages/Login.jsx
@@ -0,0 +1,62 @@
+import React, { useState } from "react";
+import "../styles/form.scss";
+
+import { Link } from "react-router";
+import axios from "axios";
+
+const Login = () => {
+  const [username, setusername] = useState("");
+  const [password, setpassword] = useState("");
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    axios
+      .post(
+        "http://localhost:3000/api/auth/login",
+        {
+          username,
+          password,
+        },
+        { withCredentials: true },
+      )
+      .then((res) => {
+        console.log(res.data);
+      });
+  }
+  return (
+    <main>
+      <div className="form-container">
+        <h1>Login</h1>
+        <form onSubmit={handleSubmit}>
+          <input
+            onInput={(e) => {
+              setusername(e.target.value);
+            }}
+            type="text"
+            name="username"
+            placeholder="Enter username"
+          />
+          <input
+            onInput={(e) => {
+              setpassword(e.target.value);
+            }}
+            type="password"
+            name="password"
+            placeholder="Enter password"
+          />
+          <button>Login</button>
+        </form>
+
+        <p>
+          Don't have an account?{" "}
+          <Link className="toggleAuthForm" to="/register">
+            Register
+          </Link>
+        </p>
+      </div>
+    </main>
+  );
+};
+
+export default Login;
